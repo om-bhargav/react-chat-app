27167af26d20ab780c5f908bd82c785a0cb027e1
OM BHARGAV <ombhargav788@gmail.com>
2025-09-04 03:44:35 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLMcKeXcTsTysEmxRFSyEfFU37MCnNHkc",
-  authDomain: "react-chat-app-e94b3.firebaseapp.com",
-  projectId: "react-chat-app-e94b3",
-  storageBucket: "react-chat-app-e94b3.firebasestorage.app",
-  messagingSenderId: "601841837970",
-  appId: "1:601841837970:web:253a8aad592c5ef248434b",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
